45926ba19ea8f9bb2cb961d622257cfff2eaed1c
Thomas Dudziak <thomas@ning.com>
2012-01-22 23:09:07 -0800

    Started to bootstrapify the UI

--- lib/nagix/http.rb
+++ lib/nagix/http.rb
@@ -37,9 +37,6 @@ module Nagix
     before do
       @qsparams = Rack::Utils.parse_query(request.query_string) # route does not handle query strings with duplicate names
 
-      @filter = nil
-      @columns = nil
-
       @columns = "Columns: " + @qsparams['attribute'].join(' ') + "\n" if @qsparams['attribute'].kind_of?(Array)
       @columns = "Columns: " + @qsparams['attribute'] + "\n" if @qsparams['attribute'].kind_of?(String)
 
@@ -50,98 +47,18 @@ module Nagix
       haml :index
     end
 
-    get '/hosts/?' do
-      @hosts = @lql.query("SELECT host_name, name FROM hosts")
-      respond_to do |wants|
-        wants.html { @hosts.nil? ? not_found : haml(:hosts) }
-        wants.json { @hosts.to_json }
-      end
+    get '/hosts' do
+      haml :hosts
     end
 
-    get ('foo') do
-      if params[:host_name] != "" and params[:service_description] == ""
-        redirect "/hosts/#{params[:host_name]}/attributes"
-      elsif params[:host_name] != "" and params[:service_description] != ""
-        redirect "/hosts/#{params[:host_name]}/#{params[:service_description]}/attributes"
-      end
-
-      @hosts = @lql.query("hosts", @filter, @columns)
-      respond_to do |wants|
-        wants.html { @hosts == nil ? not_found : haml(:hosts) }
-        wants.json { @hosts.to_json }
-      end
-    end
-
-    get '/hosts/:host_name/attributes' do
-      @host_name = params[:host_name]
-      @hosts = @lql.query("SELECT * FROM hosts WHERE host_name = '#{@host_name}' OR alias = '#{@host_name}' OR address = '#{@host_name}'")
-      respond_to do |wants|
-        wants.html { @hosts == nil or @hosts.length == 0 ? halt(404, "Host not found") : haml(:host) }
-        wants.json { @hosts.to_json }
-      end
-    end
-
-    get %r{/hosts/([a-zA-Z0-9\.]+)/([a-zA-Z0-9\.\/:_-]+)/attributes} do |host_name, service_description|
-      h = @lql.query("SELECT name FROM hosts WHERE host_name = '#{host_name}' OR alias = '#{host_name}' OR address = '#{host_name}'")
-      @hosts = @lql.query("SELECT * FROM services WHERE host_name = '#{h[0]['name']}' AND description = '#{service_description}'")
-      respond_to do |wants|
-        wants.html { @hosts == nil ? halt(404, "#{host_name} Host not found") : haml(:host) }
-        wants.json { @hosts.to_json }
-      end
-    end
-
-    get %r{/hosts/([a-zA-Z0-9\.]+)/([a-zA-Z0-9\.\/:_-]+)/command/([A-Z_]+)} do |host_name, service_description, napixcmd|
-      @host_name = host_name
-      @service_description = service_description
-      @napixcmd = napixcmd
-      @napicxmd_params =  {}
-      haml :napixcmd
-#      NagiosXcmd.docurl(napixcmd) ? redirect("#{NagiosXcmd.docurl(napixcmd)}",307) : halt(404, "Nagios External Command #{napixcmd} Not Found")
-    end
-
-    put %r{/hosts/([a-zA-Z0-9\.]+)/([a-zA-Z0-9\.\/:_-]+)/command/([A-Z_]+)} do |host_name, service_description, napixcmd|
-      begin
-        napixcmd_params = JSON.parse(request.body.read)
-        napixcmd_params[:host_name] = host_name
-        napixcmd_params[:service_description] = service_description
-      rescue JSON::ParserError
-        halt 400, "JSON parse error\n"
-      end
-      begin
-        @lql.execute(napixcmd, napixcmd_params)
-      rescue NagiosXcmd::Error => e
-        halt 400, e.message
-      end
-    end
-
-    put %r{/hosts/([a-zA-Z0-9\.]+)/command/([A-Z_]+)} do |host_name, napixcmd|
-      begin
-        napixcmd_params = JSON.parse(request.body.read)
-        napixcmd_params[:host_name] = host_name
-      rescue JSON::ParserError
-        halt 400, "JSON parse error\n"
-      end
-      begin
-        @lql.execute(napixcmd, napixcmd_params)
-      rescue NagiosXcmd::Error => e
-        halt 400, e.message
-      end
-    end
-
-    post '/hosts' do
-      if params[:host_name] != "" and params[:service_description] == ""
-        redirect "/hosts/#{params[:host_name]}/attributes"
-      elsif params[:host_name] != "" and params[:service_description] != ""
-        redirect "/hosts/#{params[:host_name]}/#{params[:service_description]}/attributes"
-      else
-        redirect "/"
-      end
+    get '/services' do
+      haml :services
     end
 
     get '/nagios' do
       @items = @lql.query("SELECT * FROM status")
       respond_to do |wants|
-        wants.html { @items == nil ? not_found : haml(:table) }
+        wants.html { @items == nil ? not_found : haml(:nagios) }
         wants.json { @items.to_json }
       end
     end
